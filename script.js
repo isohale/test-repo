eda30df0e5fd93a3496397e975e9c364cbcc1150
Catherine Griffiths <catherine-griffiths@hotmail.com>
2025-07-04 19:37:49 -0400

    update

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-// The setup code section runs once at launch
-function setup() {
-  // prepare a 2-D canvas
-  // the variables define the x and y axes of the canvas
-  createCanvas(640, 480);   
-}
+// Create a new p5.js instance for the first canvas
+const sketch1 = new p5((p) => {
+  // The setup code section runs once at launch
+  p.setup = function() {
+    // prepare a 2-D canvas
+    // the variables define the x and y axes of the canvas
+    p.createCanvas(640, 480, p.P2D, 'canvas-container-1');   
+  };
 
-// The draw code section runs 60 frames per second 
-function draw() {
-  // this variable defines the background color - it is set to blue
-  background(0,0,250);
-  // you add your drawing code below
-}
+  // The draw code section runs 60 frames per second 
+  p.draw = function() {
+    // this variable defines the background color - it is set to blue
+    p.background(0,0,250);
+    // you add your drawing code below
+  };
+});
